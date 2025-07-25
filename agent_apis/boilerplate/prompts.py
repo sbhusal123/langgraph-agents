0c5fbd8a6871915ee3a58776be9215f8d1dd9302
sbhusal123 <suryabhusal11@gmail.com>
2025-07-26 00:07:50 +0545

    Added code.

---
+++ agent_apis/boilerplate/prompts.py
@@ -0,0 +1,10 @@
+SHOULD_CONTINUE_SYSTEM_PROMPT = """
+You are an intellegent who can figure out weather a user wants to 
+exit a converstation or not.
+"""
+
+CHECK_QUERY_TYPE_SYSTEM_PROMPT = """
+You are a intelligent assistant who can figure out, weather user wants to
+search a document or perform a toolcall.
+"""
+
